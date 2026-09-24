fd865bfe52a7883e418eab6e35fbdb1c7d443b7f
gilbetjuliesa-crypto <gilbetjuliesa@gmail.com>
2026-09-24 15:10:06 +0700

    71251210
    Added functionality to calculate average grades and sort students using Merge Sort.

--- ugSorting/divcon_2_soal.py
+++ ugSorting/divcon_2_soal.py
@@ -14,32 +14,97 @@ mahasiswa = [
 
 
 # Menghitung rata-rata nilai setiap mahasiswa
-
+for rata_rata_mahasiswa in mahasiswa:
+    rata_rata_mahasiswa["rata-rata"] = sum(rata_rata_mahasiswa["nilai"]) / len(rata_rata_mahasiswa["nilai"])
 
 
 # Divide and Conquer - Merge Sort
 def merge_sort(data):
-    pass
+    if len(data) <= 1:  
+        return data 
+    
+    mid = len(data) // 2 
+    left = data[:mid]  
+    right = data[mid:]
 
+    left = merge_sort(left)  
+    right = merge_sort(right)
+
+    return merge(left, right)
 
 def merge(kiri, kanan):
-    pass
+    result = []
+    i = 0
+    j = 0 
+    while i < len(kiri) and j < len(kanan):  
+        if kiri[i]["rata-rata"] >=  kanan[j]["rata-rata"]:  
+            result.append(kiri[i])
+            i += 1  
+        else:  
+            result.append(kanan[j])  
+            j += 1
+
+    while i < len(kiri):
+        result.append(kiri[i])
+        i += 1 
+
+    while j < len(kanan):
+        result.append(kanan[j])
+        j += 1 
+
+    return result
 
 
 # Menghitung rata-rata keseluruhan
+total_rata2 = 0
+for rata_rata_mahasiswa in mahasiswa:
+    total_rata2 += rata_rata_mahasiswa["rata-rata"]
+
+rata_k = total_rata2 / len(mahasiswa)
 
 
 # Mengurutkan mahasiswa menggunakan Merge Sort
 mahasiswa_urut = merge_sort(mahasiswa)
+atas = []
+bawah = []
 
+for rata_rata_mahasiswa in mahasiswa_urut:
+    if rata_rata_mahasiswa["rata-rata"] >= rata_k:
+        atas.append(rata_rata_mahasiswa)
+    else:
+        bawah.append(rata_rata_mahasiswa)
 
 # Menampilkan hasil rata-rata keseluruhan
+print("rata-rata keseluruhan: ", rata_k)
 
 print("\n=== DI ATAS / SAMA DENGAN RATA-RATA ===")
 
 # Tampilkan List di atas / sama dengan rata-rata
-
+nomor = 1 
+for rata_rata_mahasiswa in atas:
+    print(
+        nomor,
+        ".",
+        rata_rata_mahasiswa["nama"],
+        "-nilai: ",
+        rata_rata_mahasiswa["nilai"],
+        "-Rata-rata: ",
+        rata_rata_mahasiswa["rata-rata"]
+    )
+    nomor += 1
 
 print("\n=== DI BAWAH RATA-RATA ===")
 
 # Tampilkan List di bawah rata-rata
+nomor = 1
+for rata_rata_mahasiswa in bawah:
+    print(
+        nomor,
+        ".",
+        rata_rata_mahasiswa["nama"],
+        "-nilai: ",
+        rata_rata_mahasiswa["nilai"],
+        "-Rata-rata: ",
+        rata_rata_mahasiswa["rata-rata"]
+    )
+    nomor += 1
